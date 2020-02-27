2b382b752eb19260fb3a936c864abed5f25cd0b3
XBoom <728081687@qq.com>
2020-02-28 02:11:17 +0800

    Makefile

---
+++ apps/hello.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "libtest.h"
+#include "cJSON.h"
+
+
+#define JSON_CON_LEN 1000
+void test_macro()
+{
+	#ifdef TEST_A
+		printf("hello\n");
+	#endif
+
+	#if TEST_B == 2
+		printf("world\n");
+	#endif
+}
+
+int main(void)
+{
+	test_macro();
+	int a = 10, b = 5;
+	printf("a + b = %d\n", add(a,b));
+    printf("a - b = %d\n", sub(a,b));
+    return 0;
+}
---
+++ include/libtest.h
@@ -0,0 +1,5 @@
+#ifndef __LIBTEST_H
+#define __LIBTEST_H
+int sub(int a, int b);
+int add(int a, int b);
+#endif
